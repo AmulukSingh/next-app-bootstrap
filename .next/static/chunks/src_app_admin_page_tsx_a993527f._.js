6b384bd497385e39a5a1d0341a870b2f41b9f601
AmulukSingh <142198821+AmulukSingh@users.noreply.github.com>
2025-08-11 19:51:26 +0530

    Add .next/static/chunks/src_app_admin_page_tsx_a993527f._.js 1754922085670

---
+++ .next/static/chunks/src_app_admin_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d40ced3f._.js",
+  "static/chunks/node_modules_150c1401._.js"
+],
+    source: "dynamic"
+});
